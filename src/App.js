f5d54105a6ae890e174c6343b3c2db18752b752b
rarihant <rarihant@juniper.net>
2019-04-22 01:13:55 +0530

    intial changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import styles from './App.css';
+import SearchBar from 'react-search-select'
+import {allCities} from './cities.js';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      suggestions: []
+    }
+    this.handleClear = this.handleClear.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.suggestionRenderer = this.suggestionRenderer.bind(this);
+  }
+  handleClear() {
+    this.setState({
+      suggestions: []
+    });
+  }
+
+  handleChange(input) {
+    this.setState({
+      suggestions: allCities.filter(city => city.toLocaleLowerCase().startsWith(input.toLocaleLowerCase()))
+    });
+  }
+
+  handleSelection(value) {
+    if (value) {
+      console.info(`Selected "${value}"`);
+    }
+  }
+
+  handleSearch(value) {
+    if (value) {
+      console.info(`Searching "${value}"`);
+    }
+  }
+
+  suggestionRenderer(suggestion, searchTerm) {
+    return (
+      <span>
+        <strong>{suggestion}</strong>
+      </span>
+    );
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SearchBar
+        autoFocus
+        // renderClearButton
+        // renderSearchButton
+        placeholder="search and select any city"
+        onChange={this.handleChange}
+        onClear={this.handleClear}
+        onSelection={this.handleSelection}
+        onSearch={this.handleSearch}
+        suggestions={this.state.suggestions}
+        suggestionRenderer={this.suggestionRenderer}
+        styles={styles}
+      />
     );
   }
 }
