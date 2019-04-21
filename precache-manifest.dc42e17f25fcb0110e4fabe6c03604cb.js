2f96396b738cdb2e59438f42638560a7c99bb50b
rarihant <rarihant@juniper.net>
2019-04-22 01:27:46 +0530

    Updates

---
+++ precache-manifest.dc42e17f25fcb0110e4fabe6c03604cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81239c2a70906047a34",
+    "url": "/react-search-selct/static/js/runtime~main.1957f29c.js"
+  },
+  {
+    "revision": "8b627ee151fa3512d918",
+    "url": "/react-search-selct/static/js/main.60b4dc61.chunk.js"
+  },
+  {
+    "revision": "5a287aa06ac6f45cfb92",
+    "url": "/react-search-selct/static/js/2.f9d712bb.chunk.js"
+  },
+  {
+    "revision": "8b627ee151fa3512d918",
+    "url": "/react-search-selct/static/css/main.af4bd6b8.chunk.css"
+  },
+  {
+    "revision": "d0f6130d01d8421924cd71ee019f97e4",
+    "url": "/react-search-selct/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.6606030104b8c64e81cf24426a7b651f.js"
+  "/react-search-selct/precache-manifest.dc42e17f25fcb0110e4fabe6c03604cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react/index.html", {
+workbox.routing.registerNavigationRoute("/react-search-selct/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
